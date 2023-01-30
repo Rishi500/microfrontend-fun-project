46f9033ad8b061b40d5b10efbeb9cb4d125a638c
Rishi Jain <rishi.jain500@gmail.com>
2023-01-30 10:33:20 +0530

    marketing basic setup

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// mount function to start the app.
+
+export const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+// if running in isolation, call mount fn immediately
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#_marketing-dev-root");
+  if (el) {
+    mount(el);
+  }
+}
+// if running through container, export the mount function and it will be called from container instead.
