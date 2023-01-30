8a0953f2d832974fadaaf0aadc572851629c350d
Rishi Jain <rishi.jain500@gmail.com>
2023-01-31 00:20:05 +0530

    test commit to trigger build

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+
 // mount function to start the app.
 
 export const mount = (el) => {
   ReactDOM.render(<App />, el);
 };
 
-// if running in isolation, call mount fn immediately
+// if running in isolation, call mount fn immediately.
 if (process.env.NODE_ENV === "development") {
   const el = document.querySelector("#_marketing-dev-root");
   if (el) {
