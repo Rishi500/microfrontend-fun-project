dd76bf6a064b5d9f6a46e2a4df24d2454f8ed15c
Rishi Jain <rishi.jain500@gmail.com>
2023-01-31 00:33:47 +0530

    trigger deployment

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import App from "./App";
 
-// mount function to start the app.
+// mount function to start the app
 export const mount = (el) => {
   ReactDOM.render(<App />, el);
 };
 
 // if running in isolation, call mount fn immediately.
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV == "development") {
   const el = document.querySelector("#_marketing-dev-root");
   if (el) {
     mount(el);
