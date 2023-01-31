f5476a64cc2fb2572f607bab545da9568c72fe45
Rishi Jain <rishi.jain500@gmail.com>
2023-01-31 22:54:25 +0530

    routing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { mount } from "marketing/MarketingApp";
-
+import { useHistory } from "react-router-dom";
 export default () => {
   const ref = React.useRef(null);
+  const history = useHistory();
   React.useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname: currentPathname } = history.location;
+        if (nextPathname !== currentPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref}></div>;
